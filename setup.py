cdd7e124847ed7b4820b0fcddc6ce868eb096d1d
Roopesh Deepthimahanthi <41327891+Roopesh-DMahanthi@users.noreply.github.com>
2021-02-28 18:03:30 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['numpy',
+'pandas',
+'tensorflow',
+'scikit-learn','nltk',
+'transformers']
+
+setup(
+    name='trainer',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='My training application package.'
+)
